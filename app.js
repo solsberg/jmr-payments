d262408acd147388da109a2896e61b9ab5b89685
Simon Olsberg <simon@olsbergfamily.net>
2018-01-31 19:14:55 -0500

    Refactor into multiple methods and more request validation

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const generalServerErrorMessage = "There was a problem with the payment processo
   "Please contact registration@menschwork.org for help.";
 
 api.post("/charge", (request) => {
+  console.log("POST /charge: ", request.body);
   const stripe = stripeApi(request.env.stripe_secret_api_key);
   if (!initializedVersion || initializedVersion != request.env.lambdaVersion) {
     const firebaseServiceAccount = require(`config/firebaseAccountConfig-${request.env.lambdaVersion}.json`);
@@ -23,74 +24,111 @@ api.post("/charge", (request) => {
   const eventRegRef = db.ref(`event-registrations/${request.body.eventid}/${request.body.userid}`);
 
   //TODO validate inputs
-  //validate firebase user token
-  //fetch event registration statement
-  //validate charge
-  //  - if early deposit, check not already made
-  //post charge to stripe
-  //on success, insert payment in firebase
-  //return response
 
-  console.log("verifying id token with firebase", request.body.idToken);
-  return firebaseAdmin.auth().verifyIdToken(request.body.idToken)
-  .then(decodedToken => {
-    console.log("received decodedToken:", decodedToken);
-    return new Promise((resolve, reject) => {
-      if (decodedToken.uid != request.body.userid) {
-        console.log("userid in request does not match id token");
-        reject(generalServerErrorMessage);
-        return;
-      }
-      console.log("sending charge request to stripe");
-      stripe.charges.create({
-        amount: request.body.amount * 100,
-        currency: "usd",
-        source: request.body.token,
-        description: request.body.description
-      }, function(err, charge) {
-        if (err) {
-          console.log("received error from stripe", err);
-          if (err.type === 'StripeCardError') {
-            reject("There was a problem charging your card: " + err.message);
-          } else {
-            reject(generalServerErrorMessage);
-          }
-        } else {
-          console.log("successful charge request to stripe");
-          resolve(charge);
-        }
-      });
-    });
-  }).then((charge) => {
-    return new Promise((resolve, reject) => {
-      console.log("writing charge to firebase");
-      eventRegRef.child('transactions').push({charge}, err => {
-        if (err) {
-          console.log("received error from firebase", err);
-          reject(generalServerErrorMessage);
-        } else {
-          console.log("successful write request to firebase");
-          resolve();
-        }
-      });
-    });
-  }).then(() => {
-    return new Promise((resolve, reject) => {
-      console.log("updating registration status in firebase");
-      eventRegRef.update({madeEarlyDeposit: true}, err => {
-        if (err) {
-          console.log("received error from firebase", err);
-          reject(generalServerErrorMessage);
-        } else {
-          console.log("successful update request to firebase");
-          resolve();
-        }
-      });
-    });
-  }).then(() => {
-    return "OK";
-  }).catch(err => {
+  return authenticateRequest(request)
+  .then(() => validateRegistrationState(eventRegRef))
+  .then(() => createCharge(stripe, request))
+  .then((charge) => Promise.all([
+    saveChargeData(eventRegRef, charge),
+    updateRegistration(eventRegRef, {madeEarlyDeposit: true})
+  ]))
+  .then(() => "OK")
+  .catch(err => {
     console.log(err);
+    if (err instanceof Object && !(err instanceof Error)) {
+      return new api.ApiResponse(err, {'Content-Type': 'application/json'}, err.expected ? 403 : 500);
+    }
     throw err;
   });
 });
+
+function createUserError(userMessage, expected) {
+  return {
+    userMessage,
+    expected
+  };
+}
+
+function authenticateRequest(request) {
+  console.log("verifying id token with firebase");
+  return firebaseAdmin.auth().verifyIdToken(request.body.idToken)
+  .then(decodedToken => {
+    return new Promise((resolve, reject) => {
+      if (decodedToken.uid != request.body.userid) {
+        console.log("userid in request does not match id token");
+        reject(createUserError(generalServerErrorMessage));
+      } else {
+        resolve();
+      }
+    });
+  });
+}
+
+function validateRegistrationState(eventRegRef, request) {
+  console.log("validating registration state is valid for charge request");
+  return eventRegRef.once('value')
+  .then((snapshot) => {
+    return new Promise((resolve, reject) => {
+      if (snapshot.val() && snapshot.val().madeEarlyDeposit) {
+        console.log("early deposit payment already recorded for this registration");
+        reject(createUserError(generalServerErrorMessage));
+      } else {
+        resolve();
+      }
+    });
+  });
+}
+
+function createCharge(stripe, request) {
+  console.log("sending charge request to stripe");
+  return new Promise((resolve, reject) => {
+    stripe.charges.create({
+      amount: request.body.amount * 100,
+      currency: "usd",
+      source: request.body.token,
+      description: request.body.description
+    }, function(err, charge) {
+      if (err) {
+        console.log("received error from stripe", err);
+        if (err.type === 'StripeCardError') {
+          reject(createUserError("There was a problem charging your card: " + err.message, true));
+        } else {
+          reject(createUserError(generalServerErrorMessage));
+        }
+      } else {
+        console.log("successful charge request to stripe");
+        resolve(charge);
+      }
+    });
+  });
+}
+
+function saveChargeData(eventRegRef, charge) {
+  console.log("writing charge to firebase");
+  return new Promise((resolve, reject) => {
+    eventRegRef.child('transactions').push({charge}, err => {
+      if (err) {
+        console.log("received error from firebase", err);
+        reject(createUserError(generalServerErrorMessage));
+      } else {
+        console.log("successful write request to firebase");
+        resolve();
+      }
+    });
+  });
+}
+
+function updateRegistration(eventRegRef, values) {
+  console.log("updating registration in firebase");
+  return new Promise((resolve, reject) => {
+    eventRegRef.update(values, err => {
+      if (err) {
+        console.log("received error from firebase", err);
+        reject(createUserError(generalServerErrorMessage));
+      } else {
+        console.log("successful update request to firebase");
+        resolve();
+      }
+    });
+  });
+}
